adb9eeef6d46d647c3d2902cfcfbc61f394b1411
Solah <solah.kyle.skye@gmail.com>
2024-08-19 00:02:46 -0500

    Update signHover.js

--- sks/tuneUpMy6string/scripts/signHover.js
+++ sks/tuneUpMy6string/scripts/signHover.js
@@ -1,5 +1,9 @@
 // SIGN FOR MENU
 menu.addEventListener("mouseover", sign);
+
+let bs = document.getElementById("audio-button6")
+bs.addEventListener("mouseover", sign);
+
 function sign() {
   document.getElementById("sign").style.visibility = "visible";
   setTimeout(rehideSign, 2000);}
@@ -7,8 +11,11 @@ function rehideSign() {
   document.getElementById("sign").style.visibility = "hidden";
 }
 
+
+
 //LOVES PETS SIGN
 myCat.addEventListener("mouseover", petSign);
+
 function petSign() {
   document.getElementById("pets").style.visibility = "visible";
   setTimeout(rehidePetSign, 1000);
